636415693e457e90c8bdc7313aa6be8f1e7aa3d6
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2025-03-22 18:17:30 +0700

    fix reponsive navbar

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,23 +1,29 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { userApiInstance } from "@/utils/axios.config";
-import { serialize } from "cookie";
+import {NextApiRequest, NextApiResponse} from 'next';
+import {userApiInstance} from "@/utils/axios.config";
+import {serialize} from "cookie";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         try {
-            const { email, password, keepLogin } = req.body;
+            const {email, password, keepLogin} = req.body;
 
             if (!email || !password) {
-                return res.status(400).json({ message: 'Username and password are required' });
+                return res.status(400).json({message: 'Username and password are required'});
             }
 
+            const requestData = {
+                username: email,
+                password,
+                keepLogin: keepLogin.toString()
+            }
+
+            console.log(requestData);
             const response = await userApiInstance.post('api/user/auth/login',
-                { username : email, password, keepLogin },
-                { withCredentials: true }
+                requestData
             );
 
-            if (response.data.status !== 200) {
-                return res.status(response.data.status).json({ message: response.data.msg });
+            if (response.status !== 200) {
+                return res.status(response.data).json({message: response.data});
             }
 
             const token = response.data.token;
@@ -29,10 +35,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 path: '/',
             }));
 
-            res.status(200).json({ user: response.data.user });
+            res.status(200).json({user: response.data.user});
         } catch (error) {
             console.error(error);
-            res.status(401).json({ message: 'Invalid credentials' });
+            res.status(401).json({message: 'Invalid username or password'});
         }
     } else {
         res.setHeader('Allow', ['POST']);
