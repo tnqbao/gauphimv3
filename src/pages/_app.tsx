d4a2375b17983b5ee3e469382e6886097e09d04c
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2025-03-23 19:26:33 +0700

    update authSliice, fix lost data when reload after login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import "@/styles/globals.css";
 import type {AppProps} from "next/app";
-import {ThemeProvider} from "@/components/content/theme-provider";
+import {ThemeProvider} from "@/components/provider/theme-provider";
 import {store} from "@/store";
 import {Provider} from "react-redux";
+import AuthProvider from "@/components/provider/auth-provider";
 
 export default function App({Component, pageProps}: AppProps) {
     return (
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
             <Provider store={store}>
-            <Component {...pageProps} />
+            <AuthProvider >
+                <Component {...pageProps} />
+            </AuthProvider>
             </Provider>
         </ThemeProvider>);
 }
